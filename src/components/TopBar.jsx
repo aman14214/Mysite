a2682e998c95b1c7f9a19b38c05701fe2fca0574
Your Name <you@example.com>
2025-08-20 03:52:43 +0530

    Fix Topbar styles and NavLink active state

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { FaBriefcase, FaLink, FaUser } from "react-icons/fa"; // icons
+import { FaBriefcase, FaLink, FaUser } from "react-icons/fa";
 import "./Topbar.css";
 
 export default function Topbar() {
   return (
     <nav className="topbar">
       <div className="topbar-left">
-        <NavLink to="/job-careers" className="topbar-link">
+        <NavLink
+          to="/job-careers"
+          className={({ isActive }) =>
+            `topbar-link ${isActive ? "active" : ""}`
+          }
+        >
           <FaBriefcase className="icon" /> Jobs & Careers
         </NavLink>
-        <NavLink to="/product-finder" className="topbar-link">
-          <FaLink className="icon" /> Product finder
+        <NavLink
+          to="/product-finder"
+          className={({ isActive }) =>
+            `topbar-link ${isActive ? "active" : ""}`
+          }
+        >
+          <FaLink className="icon" /> Product Finder
         </NavLink>
-        <NavLink to="/login" className="topbar-link">
+        <NavLink
+          to="/login"
+          className={({ isActive }) =>
+            `topbar-link ${isActive ? "active" : ""}`
+          }
+        >
           <FaUser className="icon" /> My DLF
         </NavLink>
       </div>
